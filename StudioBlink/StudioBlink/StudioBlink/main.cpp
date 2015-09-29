4fadcce9cd232deeac08b1af5ae870f522bddc95
epccs <ronald.sutherland@gmail.com>
2015-09-28 22:49:29 -0700

    LED not fully off due to 5V, fix by cycling between input and output

--- StudioBlink/StudioBlink/StudioBlink/main.cpp
+++ StudioBlink/StudioBlink/StudioBlink/main.cpp
@@ -12,19 +12,22 @@
 
 int main(void)
 {
-	// RPUadpt ^1 has a red LED on PA4, pull down to turn on
-	DDRA |= (1<<PA4);
+	// RPUadpt ^1 has a red LED on PA4 connected to 5V thru 1.5k Ohm
+	// the pin is 5V tolerant as an input but will 
+	// still sink current if driven high with 3V3
+	// so this will set the output low...
+	PORTA &= ~(1<<PA4); // NOT gives 01111111, then the AND gives 0******* where * is no change
 	
     /* loop */
     while (1) 
     {
-		// Turn the led on by setting corresponding register bit high.
-		PORTA |= (1<<PA4); // or gives 1******* where * is no change
+		// Turn the led on by setting it as an output.
+		DDRA |= (1<<PA4); // OR gives 1******* where * is no change
 	
 		_delay_ms(1000);
 	
-		// Turn led off by setting corresponding register bit low.
-		PORTA &= ~(1<<PA4); // not gives 01111111, and gives 0*******
+		// Turn led off by setting it as an input.
+		DDRA &= ~(1<<PA4);
 	
 		_delay_ms(1000);
     }
