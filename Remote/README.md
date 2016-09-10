8245f0dd23d84f947db930b951dfc41a57f4b724
Ron Sutherland <ronald.sutherland@gmail.com>
2016-09-09 22:52:16 -0700

    Remote: sorting out issues
    a lot like Remote2Host on RPUftdi

--- Remote/README.md
+++ Remote/README.md
@@ -1,13 +1,208 @@
 # Remote
 
-This bus manager firmware is for an RPUadpt board, it will watch for a byte on DTR to reset the local MCU board (e.g. an RPUadpt on Irrigate7) which places it in bootloader mode.
+This bus manager firmware is for an RPUadpt board, it will watch for a byte matching the local RPU_ADDRESS on the DTR pair and when seen reset the local MCU board (e.g. an RPUadpt on Irrigate7) which places it in bootloader mode. If a non-matching byte is seen the MCU is locked out from the RX and TX pair and no reset occures.
 
 ## Overview
 
-RS485 pairs have RX driver disabled and receiver enabled, TX driver enabled and receiver disabled.
+In normal mode the RS-422 pairs have RX/TX driver and receiver enabled. The RS-485 (DTR) pair is connected to the bus manager UART and my accept bytes that are compared to the local RPU_ADDRESS or to a value for RPU_NORMAL_MODE (which connects the MCU to bus).
 
-Basicly the RX pair receiver provides output for the local MCU RX input. While the TX pair driver takes input from the loclal MCU TX output. 
+Durring lockout the RX pair receiver is disconnected from the local MCU RX input. While the TX pair driver is disconnected from the local MCU TX output.  
 
-A byte on the DTR pair will reset the local MCU. DTR_DE is held high to enable the DTR pair driver. DTR_nRE is held low to enable the DTR pair receiver. 
+A byte on the DTR pair that matches the RPU_ADDRESS will cause a reset pulse to activate the bootloader. After bootloading an I2C command can be issued to send the RPU_NORMAL_MODE byte on the DTR pair.
 
-The byte on DTR is sent from Host2Remote firmware on an RPUftdi board. The two programs are an early proof of concept and need a lot more work.
+When a byte on DTR does not match the RPU_ADDRESS it will cause the lockout condition that will last for a duration determined by the LOCKOUT_DELAY or when a RPU_NORMAL_MODE byte is seen on the DTR pair.
+
+For how I setup my Makefile toolchain <http://epccs.org/indexes/Document/DvlpNotes/LinuxBoxCrossCompiler.html>.
+
+With a ISP tool connection (set the ISP_PORT in Makefile) run 'make isp' and it should compile and then flash the MCU.
+
+```
+rsutherland@conversion:~/Samba/RPUadpt/Remote$ make isp
+avr-gcc -Os -g -std=gnu99 -Wall -ffunction-sections -fdata-sections  -DF_CPU=8000000UL   -DBAUD=19200UL -I.  -mmcu=atmega328p -c -o main.o main.c
+avr-gcc -Os -g -std=gnu99 -Wall -ffunction-sections -fdata-sections  -DF_CPU=8000000UL   -DBAUD=19200UL -I.  -mmcu=atmega328p -c -o ../lib/uart.o ../lib/uart.c
+avr-gcc -Os -g -std=gnu99 -Wall -ffunction-sections -fdata-sections  -DF_CPU=8000000UL   -DBAUD=19200UL -I.  -mmcu=atmega328p -c -o ../lib/timers.o ../lib/timers.c
+avr-gcc -Os -g -std=gnu99 -Wall -ffunction-sections -fdata-sections  -DF_CPU=8000000UL   -DBAUD=19200UL -I.  -mmcu=atmega328p -c -o ../lib/twi.o ../lib/twi.c
+avr-gcc -Wl,-Map,Remote.map  -Wl,--gc-sections  -mmcu=atmega328p main.o ../lib/uart.o ../lib/timers.o ../lib/twi.o -o Remote.elf
+avr-size -C --mcu=atmega328p Remote.elf
+AVR Memory Usage
+----------------
+Device: atmega328p
+
+Program:    3684 bytes (11.2% Full)
+(.text + .data + .bootloader)
+
+Data:        298 bytes (14.6% Full)
+(.data + .bss + .noinit)
+
+
+rm -f Remote.o main.o ../lib/uart.o ../lib/timers.o ../lib/twi.o
+avr-objcopy -j .text -j .data -O ihex Remote.elf Remote.hex
+rm -f Remote.elf
+avrdude -v -p atmega328p -c stk500v1 -P /dev/ttyACM0 -b 19200 -e -U flash:w:Remote.hex -U lock:w:0x2f:m
+
+avrdude: Version 6.2
+         Copyright (c) 2000-2005 Brian Dean, http://www.bdmicro.com/
+         Copyright (c) 2007-2014 Joerg Wunsch
+
+         System wide configuration file is "/etc/avrdude.conf"
+         User configuration file is "/home/rsutherland/.avrduderc"
+         User configuration file does not exist or is not a regular file, skipping
+
+         Using Port                    : /dev/ttyACM0
+         Using Programmer              : stk500v1
+         Overriding Baud Rate          : 19200
+         AVR Part                      : ATmega328P
+         Chip Erase delay              : 9000 us
+         PAGEL                         : PD7
+         BS2                           : PC2
+         RESET disposition             : dedicated
+         RETRY pulse                   : SCK
+         serial program mode           : yes
+         parallel program mode         : yes
+         Timeout                       : 200
+         StabDelay                     : 100
+         CmdexeDelay                   : 25
+         SyncLoops                     : 32
+         ByteDelay                     : 0
+         PollIndex                     : 3
+         PollValue                     : 0x53
+         Memory Detail                 :
+
+                                  Block Poll               Page                       Polled
+           Memory Type Mode Delay Size  Indx Paged  Size   Size #Pages MinW  MaxW   ReadBack
+           ----------- ---- ----- ----- ---- ------ ------ ---- ------ ----- ----- ---------
+           eeprom        65    20     4    0 no       1024    4      0  3600  3600 0xff 0xff
+           flash         65     6   128    0 yes     32768  128    256  4500  4500 0xff 0xff
+           lfuse          0     0     0    0 no          1    0      0  4500  4500 0x00 0x00
+           hfuse          0     0     0    0 no          1    0      0  4500  4500 0x00 0x00
+           efuse          0     0     0    0 no          1    0      0  4500  4500 0x00 0x00
+           lock           0     0     0    0 no          1    0      0  4500  4500 0x00 0x00
+           calibration    0     0     0    0 no          1    0      0     0     0 0x00 0x00
+           signature      0     0     0    0 no          3    0      0     0     0 0x00 0x00
+
+         Programmer Type : STK500
+         Description     : Atmel STK500 Version 1.x firmware
+         Hardware Version: 2
+         Firmware Version: 1.18
+         Topcard         : Unknown
+         Vtarget         : 0.0 V
+         Varef           : 0.0 V
+         Oscillator      : Off
+         SCK period      : 0.1 us
+
+avrdude: AVR device initialized and ready to accept instructions
+
+Reading | ################################################## | 100% 0.02s
+
+avrdude: Device signature = 0x1e950f (probably m328p)
+avrdude: safemode: hfuse reads as DE
+avrdude: safemode: efuse reads as FD
+avrdude: erasing chip
+avrdude: reading input file "Remote.hex"
+avrdude: input file Remote.hex auto detected as Intel Hex
+avrdude: writing flash (3684 bytes):
+
+Writing | ################################################## | 100% 4.08s
+
+avrdude: 3684 bytes of flash written
+avrdude: verifying flash memory against Remote.hex:
+avrdude: load data flash data from input file Remote.hex:
+avrdude: input file Remote.hex auto detected as Intel Hex
+avrdude: input file Remote.hex contains 3684 bytes
+avrdude: reading on-chip flash data:
+
+Reading | ################################################## | 100% 2.32s
+
+avrdude: verifying ...
+avrdude: 3684 bytes of flash verified
+avrdude: reading input file "0x2f"
+avrdude: writing lock (1 bytes):
+
+Writing | ################################################## | 100% 0.03s
+
+avrdude: 1 bytes of lock written
+avrdude: verifying lock memory against 0x2f:
+avrdude: load data lock data from input file 0x2f:
+avrdude: input file 0x2f contains 1 bytes
+avrdude: reading on-chip lock data:
+
+Reading | ################################################## | 100% 0.01s
+
+avrdude: verifying ...
+avrdude: 1 bytes of lock verified
+
+avrdude: safemode: hfuse reads as DE
+avrdude: safemode: efuse reads as FD
+avrdude: safemode: Fuses OK (E:FD, H:DE, L:E2)
+
+avrdude done.  Thank you.
+```
+
+## Addressing
+
+This firmware is at Address '1' on the RPU_BUS, (not 0x1 but the ASCII value 0x31 for the character).
+
+When DTR toggles on an RPUftdi its Host2Remote firmware will send an address byte over the DTR pair. If the address sent matches the value in RPU_ADDRESS then the MCU RX/TX will be connected, for all other address the local MCU will be locked out. After a period of time in lockout or when a normal mode byte is seen, the MCU is (re)connected to RX/TX.
+
+## I2C/TWI Slave
+
+The I2C address is 0x29 (dec 41). It is organized as an array of read or write commands. Note: the sent data is used to size the reply, so add an extra byte after the command to size the reply.
+
+0: reads this shields RPU_BUS addrss (default is '1', or from EE_PROM)
+1: writes this shields RPU_BUS address to EE_PROM (not implemented)
+2: read the address sent when DTR/RTS toggles 
+3: write the address that will be sent when DTR/RTS toggles
+4: reads TBD (not implemented)
+5: wrtie normal RPU_BUS mode (send 0xFF on DTR pair) that will cancel lockout and connect all devices to the bus.
+6: reads error status bits[0:DTR readback timeout, 1:twi transmit fail, 2:DTR readback not match]
+7: wrties (or clears) error status 
+
+
+Connect to i2c-debug on an RPUno with an RPUftdi shield using picocom (or ilk). 
+
+``` 
+picocom -b 115200 /dev/ttyUSB0
+``` 
+
+Scan for the I2C slave address of the RPUftdi shield and set the byte that is sent when DTR/RTS toggles ('1' is 0x31 or 49).
+
+``` 
+/0/id?
+{"id":{"name":"I2Cdebug","desc":"RPUno Board /w atmega328p and LT3652","avr-gcc":"4.9"}}
+/0/scan?
+{"scan":[{"addr":"0x29"}]}
+/0/address 41
+{"address":"0x29"}
+/0/buffer 3,49
+{"txBuffer":[{"data":"0x3"},{"data":"0x31"}]}
+/0/read? 2
+{"rxBuffer":[{"data":"0x3"},{"data":"0x31"}]}
+``` 
+
+exit picocom with C-a, C-x. 
+
+Connect with picocom again. 
+
+``` 
+picocom -b 115200 /dev/ttyUSB0
+``` 
+
+This will toggle DTR on the RPUftdi shield which should send 0x31 on the DTR pair. The RPUftdi shield should blink slow to indicate lockout, while the RPUadpt blinks normal to indicate bootloader mode. The lockout should timeout after a delay that can be adjusted in firmware (Host2Remote or Remote) as needed.
+
+Now connect to i2c-debug on an Irrigate7 with an RPUadpt shield. The local RPU address can be read.
+
+``` 
+/1/buffer 0,255
+{"txBuffer":[{"data":"0x0"},{"data":"0xFF"}]}
+/1/read? 2
+{"rxBuffer":[{"data":"0x0"},{"data":"0x31"}]}
+``` 
+
+Set normal mode to connect all devices to the RPU_BUS.
+
+``` 
+/1/buffer 5,255
+{"txBuffer":[{"data":"0x5"},{"data":"0xFF"}]}
+/1/read? 2
+{"rxBuffer":[{"data":"0x5"},{"data":"0xFF"}]}
+``` 
--- Remote/main.c
+++ Remote/main.c
@@ -16,41 +16,141 @@ http://www.gnu.org/licenses/gpl-2.0.html
 #include <util/delay.h>
 #include <avr/io.h>
 #include "../lib/timers.h"
+#include "../lib/twi.h"
 #include "../lib/uart.h"
 #include "../lib/pin_num.h"
 #include "../lib/pins_board.h"
 
-#define BLINK_DELAY 500
+//slave address, use numbers between 0x08 to 0x78
+#define TWI_ADDRESS 0x29
+// 0x31 is ascii char for the letter '1', which may be used as second char of command e.g. /1/id?
+#define RPU_ADDRESS '1'  
+// default address sent on DTR pair if FTDI_nDTR toggles
+#define RPU_HOST_CONNECT '0' 
+// byte sent on DTR pair when FTDI_nDTR is no longer active
+#define RPU_HOST_LOCKED ~RPU_HOST_CONNECT
+// return to normal mode address sent on DTR pair (haha... no it is not oFF it is a hex value)
+#define RPU_NORMAL_MODE 0xFF
+
+static uint8_t rxBuffer[TWI_BUFFER_LENGTH];
+static uint8_t rxBufferLength = 0;
+static uint8_t txBuffer[TWI_BUFFER_LENGTH];
+static uint8_t txBufferLength = 0;
+
+#define BOOTLOADER_ACTIVE 2000
+#define BLINK_BOOTLD_DELAY 75
+#define BLINK_ACTIVE_DELAY 500
+#define BLINK_LOCKOUT_DELAY 2000
+#define BLINK_ERROR_DELAY 200
+#define UART_TTL 500
 #define LOCKOUT_DELAY 30000
 
 static unsigned long blink_started_at;
 static unsigned long lockout_started_at;
+static unsigned long uart_started_at;
+static unsigned long bootloader_started_at;
 
-int main(void)
+static uint8_t bootloader_started;
+static uint8_t host_active;
+static uint8_t bootloader_address; 
+static uint8_t activate_lockout;
+static uint8_t uart_has_TTL;
+static uint8_t controlled_by_remote;
+
+volatile uint8_t error_status;
+volatile uint8_t uart_output;
+
+// called when I2C data is received. 
+void receiveEvent(uint8_t* inBytes, int numBytes) 
+{
+    // I have to save the data
+    for(uint8_t i = 0; i < numBytes; ++i)
+    {
+        rxBuffer[i] = inBytes[i];    
+    }
+    rxBufferLength = numBytes;
+    
+    // This will copy the data to the txBuffer, which will then echo back with slaveTransmit()
+    for(uint8_t i = 0; i < numBytes; ++i)
+    {
+        txBuffer[i] = inBytes[i];    
+    }
+    txBufferLength = numBytes;
+    if (txBufferLength > 1)
+    {
+        if ( (txBuffer[0] == 0) ) // TWI command to read RPU_ADDRESS
+        {
+            txBuffer[1] = RPU_ADDRESS; // '0' is 0x31
+        }
+        if ( (txBuffer[0] == 2) ) // read byte sent on DTR pair if HOST_nDTR toggles
+        {
+            txBuffer[1] = bootloader_address;
+        }
+        if ( (txBuffer[0] == 3) ) // buffer the byte that is sent on DTR pair for use when HOST_nDTR toggles
+        {
+            bootloader_address = txBuffer[1];
+        }
+        if ( (txBuffer[0] == 5) ) // send RPU_NORMAL_MODE on DTR pair
+        {
+            if ( !(uart_has_TTL) )
+            {
+                // send a byte to the UART output
+                uart_started_at = millis();
+                uart_output = RPU_NORMAL_MODE;
+                printf("%c", uart_output); 
+                uart_has_TTL = 1;
+            }
+        }
+        if ( (txBuffer[0] == 6) ) // TWI command to read error status
+        {
+            txBuffer[1] = error_status;
+        }
+        if ( (txBuffer[0] == 7) ) // TWI command to set/clear error status
+        {
+            error_status = txBuffer[1];
+        }
+    }
+}
+
+// called when I2C data is requested.
+void slaveTransmit(void) 
+{
+    // respond with an echo of the last message sent
+    uint8_t return_code = twi_transmit(txBuffer, txBufferLength);
+    if (return_code != 0)
+        error_status &= (1<<1); // bit one set 
+}
+
+void setup(void) 
 {
     pinMode(LED_BUILTIN, OUTPUT);
     digitalWrite(LED_BUILTIN, HIGH);
     pinMode(HOST_nRTS, INPUT);
+    digitalWrite(HOST_nRTS, HIGH); // with AVR when the pin DDR is set as an input setting pin high will trun on a weak pullup 
     pinMode(HOST_nCTS, OUTPUT);
     digitalWrite(HOST_nCTS, LOW);
     pinMode(HOST_nDTR, INPUT);
+    digitalWrite(HOST_nDTR, HIGH); // another weak pullup 
     pinMode(HOST_nDSR, OUTPUT);
     digitalWrite(HOST_nDSR, LOW);
     pinMode(RX_DE, OUTPUT);
-    digitalWrite(RX_DE, HIGH);  // allow RX pair driver to enable if HOST_TX is low
+    digitalWrite(RX_DE, HIGH);  // allow RX pair driver to enable if FTDI_TX is low
     pinMode(RX_nRE, OUTPUT);
     digitalWrite(RX_nRE, LOW);  // enable RX pair recevior to output to local MCU's RX input
     pinMode(TX_DE, OUTPUT);
-    digitalWrite(TX_DE, LOW); // disallow TX pair driver to enable if TX (from MCU) is low
+    digitalWrite(TX_DE, HIGH); // allow TX pair driver to enable if TX (from MCU) is low
     pinMode(TX_nRE, OUTPUT);
-    digitalWrite(TX_nRE, LOW);  // enable TX pair recevior to output to HOST_RX input
+    digitalWrite(TX_nRE, LOW);  // enable TX pair recevior to output to FTDI_RX input
     pinMode(DTR_DE, OUTPUT);
-    digitalWrite(DTR_DE, LOW);  // disallow DTR pair driver to enable if DTR_TXD is low
+    digitalWrite(DTR_DE, LOW);  // seems to be a startup glitch ??? so disallow DTR pair driver to enable if DTR_TXD is low
     pinMode(nSS, OUTPUT); // nSS is input to a Open collector buffer used to pull to MCU nRESET low
     digitalWrite(nSS, HIGH); 
 
-    uint8_t activate_bootloader = 0;
-    uint8_t activate_lockout = 0;
+    bootloader_address = RPU_HOST_CONNECT; 
+    host_active = 0;
+    activate_lockout = 0;
+    uart_has_TTL = 0;
+    error_status = 0;
 
     //Timer0 Fast PWM mode, Timer1 & Timer2 Phase Correct PWM mode.
     initTimers(); 
@@ -58,65 +158,217 @@ int main(void)
     /* Initialize UART, it returns a pointer to FILE so redirect of stdin and stdout works*/
     stdout = stdin = uartstream0_init(BAUD);
 
+    twi_setAddress(TWI_ADDRESS);
+    twi_attachSlaveTxEvent(slaveTransmit); // called when I2C data is requested 
+    twi_attachSlaveRxEvent(receiveEvent); // slave receive
+    twi_init(false); // do not use internal pull-up
+
     sei(); // Enable global interrupts to start TIMER0 and UART
-
-    uart0_flush(); // dump the transmit buffer
+    
+    _delay_ms(50); // wait for UART glitch to clear
     digitalWrite(DTR_DE, HIGH);  // allow DTR pair driver to enable if DTR_TXD is low
+}
 
-    blink_started_at = millis();
-
-    while (1) 
+// blink if the host is active, fast blink if error status, slow blink in lockout
+void blink_on_activate(void)
+{
+    unsigned long kRuntime = millis() - blink_started_at;
+    
+    if (!error_status) 
     {
-        unsigned long kRuntime = 0;
-        
-        kRuntime = millis() - blink_started_at;
-        
-        if ( activate_bootloader & (kRuntime > BLINK_DELAY) )
+        if ( bootloader_started  && (kRuntime > BLINK_BOOTLD_DELAY) )
         {
             digitalToggle(LED_BUILTIN);
             
             // next toggle 
-            blink_started_at += BLINK_DELAY; 
+            blink_started_at += BLINK_BOOTLD_DELAY; 
+        }
+        else if ( activate_lockout  && (kRuntime > BLINK_LOCKOUT_DELAY) )
+        {
+            digitalToggle(LED_BUILTIN);
+            
+            // next toggle 
+            blink_started_at += BLINK_LOCKOUT_DELAY; 
+        }
+        else if ( host_active  && (kRuntime > BLINK_ACTIVE_DELAY) )
+        {
+            digitalToggle(LED_BUILTIN);
+            
+            // next toggle 
+            blink_started_at += BLINK_ACTIVE_DELAY; 
+        }
+        // else spin the loop
+    }
+    else
+    {
+        if ( (kRuntime > BLINK_ERROR_DELAY))
+        {
+            digitalToggle(LED_BUILTIN);
+            
+            // next toggle 
+            blink_started_at += BLINK_ERROR_DELAY; 
+        }
+    }
+}
+
+void check_Bootload_Time(void)
+{
+    if (bootloader_started)
+    {
+        unsigned long kRuntime = millis() - bootloader_started_at;
+        
+        if ( kRuntime > BOOTLOADER_ACTIVE)
+        {
+            host_active =1;
+            bootloader_started = 0;
+        }
+    }
+}
+
+void check_DTR(void)
+{
+    if (!controlled_by_remote)
+    {
+        if ( !digitalRead(HOST_nDTR) )  // both HOST_nDTR and HOST_nRTS are set (active low) when avrdude tries to use the bootloader
+        { 
+            if ( !(bootloader_started  || activate_lockout || host_active || uart_has_TTL) )
+            {
+                // send a byte on the DTR pair when FTDI_nDTR is first active
+                uart_started_at = millis();
+                uart_output= bootloader_address; // set by I2C, default is RPU_HOST_CONNECT
+                printf("%c", uart_output); 
+                uart_has_TTL = 1;
+            }
+        }
+        else
+        {
+            if ( host_active && (!uart_has_TTL) && (!bootloader_started) && (!activate_lockout) )
+            {
+                // send a byte on the DTR pair when FTDI_nDTR is first non-active
+                uart_started_at = millis();
+                uart_output= RPU_HOST_LOCKED;
+                printf("%c", uart_output); 
+                uart_has_TTL = 1;
+                digitalWrite(LED_BUILTIN, HIGH);
+            }
+        }
+    }
+}
+
+// lockout needs to happoen for a long enough time to insure bootloading is finished,
+void check_lockout(void)
+{
+    unsigned long kRuntime = millis() - lockout_started_at;
+    
+    if ( activate_lockout && (kRuntime > LOCKOUT_DELAY))
+    {
+        activate_lockout =0;
+        digitalWrite(LED_BUILTIN, HIGH);
+        digitalWrite(RX_DE, HIGH); // allow RX pair driver to enable if HOST_TX is low
+        digitalWrite(RX_nRE, LOW);  // enable RX pair recevior to output to local MCU's RX input
+        digitalWrite(TX_DE, HIGH); // allow TX pair driver to enable if TX (from MCU) is low
+        digitalWrite(TX_nRE, LOW);  // enable TX pair recevior to output to HOST_RX input
+        
+        host_active = 1;
+    }
+}
+
+void check_uart(void)
+{
+    unsigned long kRuntime = millis() - uart_started_at;
+ 
+    if ( uart0_available() )
+    {
+        char input;
+        input = getchar();
+
+        // was this byte sent with the local DTR pair driver, if so the error status may need update
+        // and the lockout from a local host needs to be treated differently
+        // need to ignore the local host's DTR if getting control from a remote host
+        if ( uart_has_TTL )
+        {
+            if(input != uart_output) 
+            { // sent byte does not match,  but I'm not to sure what would cause this.
+                error_status &= (1<<2); // bit two set 
+            }
+            uart_has_TTL = 0;
+            controlled_by_remote = 0;
+        }
+        else
+        {
+            controlled_by_remote = 1;
         }
 
-        kRuntime = millis() - lockout_started_at;
-        
-        if ( activate_lockout & (kRuntime > LOCKOUT_DELAY))
-        {
+        if (input == RPU_NORMAL_MODE) // end the lockout if it was set.
+        { 
             activate_lockout =0;
-            activate_bootloader =0;
+            host_active =1;
             digitalWrite(LED_BUILTIN, HIGH);
             digitalWrite(RX_DE, HIGH); // allow RX pair driver to enable if HOST_TX is low
             digitalWrite(RX_nRE, LOW);  // enable RX pair recevior to output to local MCU's RX input
             digitalWrite(TX_DE, HIGH); // allow TX pair driver to enable if TX (from MCU) is low
             digitalWrite(TX_nRE, LOW);  // enable TX pair recevior to output to HOST_RX input
         }
-
-        if ( uart0_available() )  // a byte from DTR pair may activate bootloader or cause lockout for a time
+        else if (input == RPU_HOST_LOCKED) // the host is disconnected 
         { 
-            char input = getchar();
-            
-            if (input == '1') // Host2Remote sends a '1' when its DTR is toggled, 
+            activate_lockout =0;
+            host_active =0;
+            digitalWrite(LED_BUILTIN, HIGH);
+            digitalWrite(RX_DE, LOW); // disallow RX pair driver to enable if FTDI_TX is low
+            digitalWrite(RX_nRE, LOW);  // enable RX pair recevior to output to local MCU's RX input
+            digitalWrite(TX_DE, HIGH); // allow TX pair driver to enable if TX (from MCU) is low
+            digitalWrite(TX_nRE, HIGH);  // disable TX pair recevior that outputs to FTDI_RX input
+        }
+        else
+        {
+            if (input == RPU_ADDRESS) // that is my local address
             {
-                activate_bootloader =1;
+                // connect the host and local mcu
+                digitalWrite(RX_DE, HIGH); // allow RX pair driver to enable if FTDI_TX is low
+                digitalWrite(RX_nRE, LOW);  // enable RX pair recevior to output to local MCU's RX input
+                digitalWrite(TX_DE, HIGH); // allow TX pair driver to enable if TX (from MCU) is low
+                digitalWrite(TX_nRE, LOW);  // enable TX pair recevior to output to FTDI_RX input
+
+                // start the bootloader
+                bootloader_started = 1;
                 digitalWrite(nSS, LOW);   // nSS goes through a open collector buffer to nRESET
                 _delay_ms(20);  // hold reset low for a short time 
                 digitalWrite(nSS, HIGH); // this will release the buffer with open colllector on MCU nRESET.
-                
-                // set lockout flag to use its delay but do not disable the tranceivers
-                activate_lockout =1;
-                lockout_started_at = millis();
+                blink_started_at = millis();
+                bootloader_started_at = millis();
             }
             else
-            {
+            {  // lockout both MCU and host
+                activate_lockout =1;
                 digitalWrite(RX_DE, LOW); // disallow RX pair driver to enable if HOST_TX is low
                 digitalWrite(RX_nRE, HIGH);  // disable RX pair recevior to output to local MCU's RX input
                 digitalWrite(TX_DE, LOW); // disallow TX pair driver to enable if TX (from MCU) is low
                 digitalWrite(TX_nRE, HIGH);  // disable TX pair recevior to output to HOST_RX input
-                activate_lockout =1;
                 lockout_started_at = millis();
+                blink_started_at = millis();
             }
         }
+    }
+    else if (uart_has_TTL && (kRuntime > UART_TTL) )
+    { // perhaps the DTR line is stuck (e.g. someone has pulled it low) so may need to time out
+        error_status &= (1<<0);
+        uart_has_TTL = 0;
+    }
+}
+
+int main(void)
+{
+    setup();
+
+    blink_started_at = millis();
+
+    while (1) 
+    {
+        blink_on_activate();
+        check_Bootload_Time();
+        check_DTR();
+        check_lockout();
+        check_uart();
     }    
 }
 
